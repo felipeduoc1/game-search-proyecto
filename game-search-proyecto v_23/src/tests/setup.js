7e4b975c810eb1fafeab9e121673cd8b5f107489
Alumno Vina del Mar <vina@duoc.acad>
2025-11-28 13:07:24 -0300

    subiendo proyecto final

--- game-search-proyecto v_23/src/tests/setup.js
+++
@@ -1,8 +0,0 @@
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
-
-// Limpia el DOM después de cada prueba para evitar efectos secundarios.
-afterEach(() => {
-  cleanup();
-});
